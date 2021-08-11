af3793b1238760411b7c9b8954674cd14711a065
leslieyi <leslieyi411@gmail.com>
2021-08-10 22:31:00 -0400

    done

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    hash = {}
+    @@genres.each do |genre|
+      hash[genre] ||= 0 # if it is nil, assign 0,  a = b unless a 
+      hash[genre] += 1
+    end
+    hash
+  end
+
+  def self.artist_count
+    hash = {}
+    @@artists.each do |artist|
+      hash[artist] ||= 0 # if it is nil, assign 0,  a = b unless a 
+      hash[artist] += 1
+    end
+    hash
+  end
+end
